016b6f7f6288b910a2dedb6c6f740214a3d90ce2
ChrisJonathan22 <christophereko@hotmail.fr>
2018-03-09 21:04:50 +0000

    Gunshot Effect
    First Commit.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("click", gunshot, false);
+
+
+function gunshot (event){
+        // 1. Clone the img element
+        var el = document.getElementById("gunshot").cloneNode();
+        // 2. Append it to the HTML document
+        document.body.appendChild(el);
+        // 3. Remove the display:none style
+        el.style.display = "";
+        // 4. Show image at the mouse coordinates
+        el.style.left = event.clientX - (el.offsetWidth / 2) + "px";
+        el.style.top = event.clientY - (el.offsetHeight / 2) + "px";
+        // 5. Add sound effect
+        var shotSound = new Audio();
+        shotSound.src = "resources/gunsound.mp3";
+        shotSound.play();
+
+}
